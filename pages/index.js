a491f8f6bc832a317cf7014c3d0b8a61b6d464d6
cosmin199 <cosmin_ionescu_email@yahoo.ro>
2021-09-27 01:35:11 -0700

    blog page updated

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,10 @@
 import Container from "../components/Container"
-import {
-  Heading,
-  Text,
-  Button
-} from '@chakra-ui/react'
-import { NextSeo } from 'next-seo'
+import { Heading, Text, Button } from "@chakra-ui/react"
+import { NextSeo } from "next-seo"
 
-const url = 'https://example.io/'
-const title = 'Home'
-const description = 'Awesome description.'
+const url = "https://example.io/"
+const title = "Home"
+const description = "Awesome description."
 
 export default function Home() {
   return (
@@ -20,13 +16,19 @@ export default function Home() {
         openGraph={{
           url,
           title,
-          description
+          description,
         }}
       />
       <Container>
-        <Heading as="h1" size="3xl">Hello, I am Benjamin Carlson</Heading>
-        <Text fontSize="2xl" my={4}>A developer, creator, and student.</Text>
-        <Button colorScheme="cyan" size="lg">Hire Me!</Button>
+        <Heading as="h1" size="3xl">
+          Hello, I am Cosmin
+        </Heading>
+        <Text fontSize="2xl" my={4}>
+          A developer, creator, and student.
+        </Text>
+        <Button colorScheme="cyan" size="lg">
+          Hire Me!
+        </Button>
       </Container>
     </>
   )
